e5be4048013a84d4f7efc313183796deaaed4954
UalNamNuhc <33993002+Ualnomis@users.noreply.github.com>
2021-04-12 03:08:01 +0800

    Update 3d_GenerateTrainerAccounts.ps1

--- 3d_GenerateTrainerAccounts.ps1
+++ 3d_GenerateTrainerAccounts.ps1
@@ -4,25 +4,38 @@ $data = Get-Content ((split-path -parent $MyInvocation.MyCommand.Definition) + "
 # get trainers data and save to $trainers
 $dictory = @() 
 $trainers = @()
+
+# read entire txt file
 for ($i = 0; $i -lt $data.Length; $i++) {
+    # read and split the row that have comma
     $rowData = $data[$i].Split(",")
+    # if $rowData array has more than 1 element
     if (!($rowData.Length -eq 1)) {
-        # Add the first row to dictory array
+        # Add the first row (Login Name,First Name,Last Name,Email,Telephone,HKID) to dictory array
         if ($dictory.Length -eq 0) {
+            # remove space
             $rowData = $rowData.Replace(' ', '')
+            # $rowData string array each string as $dict
             foreach ($dict in $rowData) {
+                #  store $dict to $dictory
                 $dictory += $dict
             }
   
         }
         else {
             # Add data associate the dictory
+            # if the $rowData have same length with $dictory (Login Name,First Name,Last Name,Email,Telephone,HKID)
             if ($dictory.Length -eq $rowData.Length) {
+                # reset trainer
                 $trainer = $null
+                
+                
                 for ($count = 0; $count -lt $dictory.Length; $count++) {
+                    # combine $dictory (Login Name,First Name,Last Name,Email,Telephone,HKID) with value to $trainer
                     $trainer += @{$dictory[$count] = $rowData[$count] }
                 }
-
+                
+                # add $trainer to trainers
                 $trainers += $trainer
 
             }
